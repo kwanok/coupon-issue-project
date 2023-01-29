cdd500a3c595eaf2a64338fe52185f531e70231b
nohkwanok <cloq@kakao.com>
2023-01-29 22:33:22 +0900

    프로젝트 초기화

--- main.go
+++ main.go
@@ -1,31 +1,16 @@
 package main
 
 import (
-	"flag"
 	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/fiber/v2/middleware/logger"
-	"github.com/kwanok/coupon-issue-project/coupon"
-	"github.com/kwanok/coupon-issue-project/rdb"
+	"log"
 )
 
-var serverPort = flag.String("serverPort", ":3000", "api server serverPort")
-var redisAddr = flag.String("redisAddr", "0.0.0.0:6379", "redis address")
-
 func main() {
-	flag.Parse()
-
-	StartServer()
-}
-
-func StartServer() {
-	redisClient := rdb.NewClient(*redisAddr)
 	app := fiber.New()
 
-	app.Use(logger.New(logger.ConfigDefault))
-	coupon.Init(app, redisClient)
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World!")
+	})
 
-	err := app.Listen(*serverPort)
-	if err != nil {
-		panic(err)
-	}
+	log.Fatal(app.Listen(":3000"))
 }
